86c191522775e06d46d15e0977964cf7c38dcd32
jagadheesh <154898803+jagadeeshm27@users.noreply.github.com>
2026-08-04 23:33:28 +0530

    Update authroutes.js

--- authroutes.js
+++ authroutes.js
@@ -1,7 +1,7 @@
 import express from 'express';
 import bcrypt from 'bcryptjs';
 import jwt from 'jsonwebtoken';
-import user from './user.js';
+import User from './user.js';
 import { requireAuth } from './auth.js';
 
 const router = express.Router();
